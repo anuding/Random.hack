ac0a7569413151f6a4f641fbc95052868d69cc29
anuding <anuding@126.com>
2017-05-29 17:34:13 +0800

    now it can show tags

--- Emotion/Emotion/MainPage.xaml.cs
+++ Emotion/Emotion/MainPage.xaml.cs
@@ -20,6 +20,9 @@ using Microsoft.ProjectOxford.Vision.Contract;
 using Windows.Graphics.Imaging;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Xaml.Media.Imaging;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -30,102 +33,119 @@ namespace Emotion
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        readonly string _subscriptionKey;
+        //readonly string _subscriptionKey;
 
         public MainPage()
         {
             //set your key here
-            _subscriptionKey = "63682cf3bc7f424aba4c2b3634682204";
+           // Console.Write("Enter image file path: ");
+            string imageFilePath = ("d:\\1.jpg");//= Console.ReadLine();
+            //string wangzhi = Input.Text;
+
+            MakeAnalysisRequest(imageFilePath);
+            //MakeAnalysisRequest(wangzhi);
+            // Console.WriteLine("\n\n\nHit ENTER to exit...");
+            //Console.ReadLine();
+            // _subscriptionKey = "63682cf3bc7f424aba4c2b3634682204";
             this.InitializeComponent();
+        }
+
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+
+        static async Task<string> MakeAnalysisRequest(string wangzhi)
+        {
+            var client = new HttpClient();
+
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "63682cf3bc7f424aba4c2b3634682204");
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "visualFeatures=Tags&language=en";
+            string uri = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
+            HttpResponseMessage response;
+            
+            byte[] byteData = Encoding.UTF8.GetBytes("{\"url\":\"" + "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1496059051033&di=257f16ede20b0dfc9256f418c7a8fb88&imgtype=0&src=http%3A%2F%2Fpic15.photophoto.cn%2F20100615%2F0006019058815826_b.jpg" + "\"}");
+            //byte[] byteData = Encoding.UTF8.GetBytes("{\"url\":\"" + wangzhi + "\"}");
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+                var r1 = await response.Content.ReadAsStringAsync();
+                // ResultsTextBlock.Text = "efewfewfew" +  "\n\n ";//r1 +
+                //Console.WriteLine(await response.Content.ReadAsStringAsync());
+                
+                return await response.Content.ReadAsStringAsync();
+            }
         }
 
         private async void AnalyzeButton_Click(object sender, RoutedEventArgs e)
         {
-            var openPicker = new FileOpenPicker
-            {
-                ViewMode = PickerViewMode.Thumbnail,
-                SuggestedStartLocation = PickerLocationId.PicturesLibrary
-            };
-            openPicker.FileTypeFilter.Add(".jpg");
-            openPicker.FileTypeFilter.Add(".jpeg");
-            openPicker.FileTypeFilter.Add(".png");
-            openPicker.FileTypeFilter.Add(".gif");
-            openPicker.FileTypeFilter.Add(".bmp");
-            var file = await openPicker.PickSingleFileAsync();
+            ResultsTextBlock.Text = await MakeAnalysisRequest("d:\\1.jpg") + "\n\n ";
+            //ImageToAnalyze.Source =
+            /* var openPicker = new FileOpenPicker
+             {
+                 ViewMode = PickerViewMode.Thumbnail,
+                 SuggestedStartLocation = PickerLocationId.PicturesLibrary
+             };
+             openPicker.FileTypeFilter.Add(".jpg");
+             openPicker.FileTypeFilter.Add(".jpeg");
+             openPicker.FileTypeFilter.Add(".png");
+             openPicker.FileTypeFilter.Add(".gif");
+             openPicker.FileTypeFilter.Add(".bmp");
+             var file = await openPicker.PickSingleFileAsync();
 
-            if (file != null)
-            {
-                //await
-                ShowPreviewAndAnalyzeImage(file);
-            }
+             if (file != null)
+             {
+                 //await
+                     ShowPreviewAndAnalyzeImage(file);
+
+             }*/
         }
 
-        private async Task ShowPreviewAndAnalyzeImage(StorageFile file)
+        /*private async Task ShowPreviewAndAnalyzeImage(string r1)
         {
-            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            //var stream1 = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
 
-            var image = new BitmapImage();
+            //var image = new BitmapImage();
+            
+            //image.SetSource(stream1);
 
-            image.SetSource(stream);
-
-            //imageView.Source = image;
-            //preview image
-            //var bitmap = await LoadImage(file);
-            //var bitmap = new RenderTargetBitmap();
-            //SharedStorageAccessManager.AddFile(file);
-            ImageToAnalyze.Source = image;
+        
+            //ImageToAnalyze.Source = image;
 
             //analyze image
-            var results = await AnalyzeImage(file);
+            //var results = await AnalyzeImage(file);
 
-            //"fr", "ru", "it", "hu", "ja", etc...
-            //var ocrResults = await AnalyzeImageForText(file, "en");
-
+           
             //parse result
             //ResultsTextBlock.Text = ParseResult(results) + "\n\n " + ParseOCRResults(ocrResults);
-            ResultsTextBlock.Text = "efewfewfew" + results + "\n\n ";// + ocrResults;
-        }
+            ResultsTextBlock.Text = "efewfewfew"  + "\n\n ";// + ocrResults;
+        }*/
 
-        private async Task<AnalysisResult> AnalyzeImage(StorageFile file)
+       /* private async Task<AnalysisResult> AnalyzeImage(StorageFile file)
         {
 
-            VisionServiceClient VisionServiceClient = new VisionServiceClient(_subscriptionKey);
+            VisionServiceClient visionServiceClient = new VisionServiceClient(_subscriptionKey);
 
-            using (Stream imageFileStream = await file.OpenStreamForReadAsync())
-            {
+            Stream imageFileStream = await file.OpenStreamForReadAsync();
+            
                 // Analyze the image for all visual features
                 VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories
             , VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType
             , VisualFeature.Tags };
-                AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures);
+                AnalysisResult analysisResult = await visionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures);
                 return analysisResult;
-            }
-        }
-
-        private async Task<OcrResults> AnalyzeImageForText(StorageFile file, string language)
-        {
-            //language = "fr", "ru", "it", "hu", "ja", etc...
-            VisionServiceClient VisionServiceClient = new VisionServiceClient(_subscriptionKey);
-            using (Stream imageFileStream = await file.OpenStreamForReadAsync())
-            {
-                OcrResults ocrResult = await VisionServiceClient.RecognizeTextAsync(imageFileStream, language);
-                return ocrResult;
-            }
-        }
-
-
-        /*public MainPage()
-        {
-            this.InitializeComponent();
-        }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MediaElement mediaElement = new MediaElement();
-            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
-            mediaElement.SetSource(stream, stream.ContentType);
-            mediaElement.Play();
+            
         }*/
+
+       
     }
 }
