cf5786ba361be3de0a848c599f0f558d698e0bde
anuding <anuding@126.com>
2017-05-30 01:03:58 +0800

    删了没用的代码，加了注释

--- Emotion/Emotion/MainPage.xaml.cs
+++ Emotion/Emotion/MainPage.xaml.cs
@@ -45,20 +45,12 @@ namespace Emotion
         public MainPage()
         {
 
-            //set your key here
-           // Console.Write("Enter image file path: ");
-            string imageFilePath = ("d:\\1.jpg");//= Console.ReadLine();
-            //
-
-            //MakeAnalysisRequest(imageFilePath);
-            //MakeAnalysisRequest(wangzhi);
-            // Console.WriteLine("\n\n\nHit ENTER to exit...");
-            //Console.ReadLine();
-            // _subscriptionKey = "63682cf3bc7f424aba4c2b3634682204";
+          //这是干嘛的我忘了(⊙——⊙)
+            string imageFilePath = ("d:\\1.jpg");
             this.InitializeComponent();
         }
 
-
+        //转流
         static byte[] GetImageAsByteArray(string imageFilePath)
         {
             FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
@@ -66,7 +58,7 @@ namespace Emotion
             return binaryReader.ReadBytes((int)fileStream.Length);
         }
 
-
+        //召唤图片分析
         static async Task<string> MakeAnalysisRequest(string wangzhi)
         {
             var client = new HttpClient();
@@ -77,6 +69,7 @@ namespace Emotion
 
             // Request parameters. A third optional parameter is "details".
             string requestParameters = "visualFeatures=Tags&language=en";
+            //endpoint一定要和key配套
             string uri = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
             HttpResponseMessage response;
             
@@ -86,33 +79,21 @@ namespace Emotion
             {
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 response = await client.PostAsync(uri, content);
-                var r1 = await response.Content.ReadAsStringAsync();
-                
-                
                 return await response.Content.ReadAsStringAsync();
             }
         }
 
         private async void AnalyzeButton_Click(object sender, RoutedEventArgs e)
         {
+            //读取输入网址
             string wangzhi = Input.Text;
-
-            //var file = await openPicker.PickSingleFileAsync();
-
-            //if (file != null)
-            {
-                //await
-              //  var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
-
-                var image = new BitmapImage();
-               // imgBitmap = new BitmapImage();
-                var uri = new System.Uri(wangzhi);
-                image.UriSource = uri;
-               // img.Source = imgBitmap;
-                //image.SetSource(stream);
-                ImageToAnalyze.Source = image;
-            }
+            //显示图片
+            var image = new BitmapImage();
+            var uri = new System.Uri(wangzhi);
+            image.UriSource = uri;
+            ImageToAnalyze.Source = image;
             
+            //显示分析结果
             ResultsTextBlock.Text = await MakeAnalysisRequest(wangzhi) + "\n\n ";
             
         }
